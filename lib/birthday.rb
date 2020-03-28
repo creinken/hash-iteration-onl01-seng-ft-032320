4b2edbac2b8ec1e892379503136bc9dbc610196a
Christopher Reinken <christopherreinken@gmail.com>
2020-03-28 00:46:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
-    if age < 13
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
