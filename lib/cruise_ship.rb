8380cfcb397626cd6a5ec82bc548b19e352e2522
Christopher Reinken <christopherreinken@gmail.com>
2020-03-28 00:40:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   passengers.each do |room, name|
-    if room == "suite_a" && name.start_with? == "A"
+    if room == :suite_a && name.start_with? == "A"
       winner = name
     end
   end
